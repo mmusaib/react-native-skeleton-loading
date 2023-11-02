3b5b68cca08cd26785f128e06a8de1e7773a3c7c
Muhammad Musaib <musaib.999@gmail.com>
2023-11-02 20:08:41 +0530

    Repository Initialized

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+
+export interface SkeletonLoading {
+    /**
+     * background of the loader componenet
+     */
+    background: string,
+
+    /**
+     * highlight color of the loader component
+     */
+    highlight: string
+}
